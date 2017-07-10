b3161dc931a13e4b3b661f0caf244e223b909604
bonattt <bonattt@rose-hulman.edu>
2017-07-10 14:27:51 -0600

    Added interfaces

---
+++ CroweHelloWorld/CroweHelloWorld/IDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweHelloWorld
+{
+    interface IDataSource
+    {
+        // Returns the next line of data from the source
+        // returns null if there is no more data to display
+        string NextLine();
+    }
+}
